84b0b757892f918cbf37fd8efbf3298cbfa99d72
onkar18829 <119295557+onkar18829@users.noreply.github.com>
2026-06-03 09:12:36 +0530

    fix: switch to netlify adapter

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
+// https://astro.com/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
